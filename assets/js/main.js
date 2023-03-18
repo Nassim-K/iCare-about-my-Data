db325c62aaad99b90d1682e17c13c7d962cf6019
Nassim-K <theskyblog3@gmail.com>
2023-03-18 18:22:12 +0100

    18/03

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-console.log('ok');
+$(document).ready(function () {
+    $(".home-content").css({
+        transform: "scale(1)",
+        opacity: "1",
+    });
+    $(".fixed-bar").css({
+        opacity: "1",
+    });
+});
