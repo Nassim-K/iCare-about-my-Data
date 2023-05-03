7f426d0f292fddc415d3857fcdc9c2a7cbc056d0
Nassim-K <theskyblog3@gmail.com>
2023-05-04 00:07:52 +0200

    Prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,29 @@
 module.exports = {
-    entry: './assets/js/app.js',
-  mode: 'development',
-    watch:true,
-    output: {
-      path: `${__dirname}/dist`,
-      filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-          {
-            test: /\.scss$/,
-            exclude: /node_modules/,
-            use: [
-              {
-                  loader: 'file-loader',
-                  options: { outputPath: 'css/', name: '[name].min.css'}
-              },
-              'sass-loader'
-            ]
+  entry: './assets/js/app.js',
+  mode: 'production',
+  watch: true,
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [{
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [{
+            loader: 'file-loader',
+            options: {
+              outputPath: 'css/',
+              name: '[name].min.css'
+            }
           },
-          {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader']
-          }
-        ],
-    },
-  };
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ],
+  },
+};
